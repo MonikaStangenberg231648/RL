88f6494b844c853dd6e969f54e755a09e76273e9
StenGni <231648@buas.nl>
2025-01-15 13:25:19 +0100

    a

--- RL/rl_train_rem.py
+++ RL/rl_train_rem.py
@@ -6,7 +6,7 @@ from clearml import Task
 import wandb
 from wandb.integration.sb3 import WandbCallback
 import os
-
+os.system("pip install typing_extensions>=4.7.0")
 os.system("pip install 'shimmy>=2.0'")
 
 os.environ['WANDB_API_KEY'] = '81e90cda052e4ff4b2e6d490e7c614a9b48a3307' 
